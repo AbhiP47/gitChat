3a4ba914c6dbd994fc22bf3c2365ffcbec094580
AbhiP47 <abhinavjaipanwar@gmail.com>
2026-09-05 19:56:11 +0530

    initial commit

---
+++ gitChat-Backend/src/main/java/com/springRag/gitChat/GitChatApplication.java
@@ -0,0 +1,13 @@
+package com.springRag.gitChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitChatApplication.class, args);
+	}
+
+}
---
+++ gitChat-Backend/src/test/java/com/springRag/gitChat/GitChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springRag.gitChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
